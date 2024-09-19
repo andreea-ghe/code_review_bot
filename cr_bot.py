8597b603b12c6cb189c9a8106e8a4d6df403c521
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-09-19 18:28:20 +0300

    Update cr_bot.py

--- cr_bot.py
+++ cr_bot.py
@@ -1,9 +1,11 @@
 import os
 import sys
 from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import Chroma
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
@@ -32,7 +34,7 @@ def generate_feedback():
         documents = loader_all_files_content.load()
     
         # Split the app's context into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
         context_chunks = text_splitter.split_documents(documents)
     
         # Create a vector store for retrieving context using OpenAI embeddings
@@ -40,9 +42,63 @@ def generate_feedback():
         retriever = vectorstore.as_retriever()
     
         ### Contextualize question ###
-        system_prompt = """You are given a chat history that contains \
-        an entire app with all the files and modules and the user will give you a diff \
-        file. Please analyze this diff in the context of the entire app, what are the \
+        # system_prompt = """You are given a chat history that contains \
+        # an entire app with all the files and modules and the user will give you a diff \
+        # file. Please analyze this diff in the context of the entire app, what are the \
+        # benefits, the drawbacks, if there was a mistake in the logic, how will this \
+        # change impact the modules that are dependent on this file. Point out any bugs \
+        # or potential issues that might appear 🐛. Suggest improvements related to \ 
+        # coding best practices. Ensure the code changes align with the descriptions in \
+        # the commit messages. Highlight any security vulnerabilities or concerns.🪪 \
+        # Focus on major issues and avoid minor stylistic preferences. \
+        # Use bullet points for multiple comments. \
+        # Be specific in your feedback and provide examples if possible."""
+
+        # PROJECT_ID = "code-review-329e5"
+        # SESSION_ID = "user_session"
+        # COLLECTION_NAME = "chat_history"
+
+        # client = firestore.client(project = PROJECT_ID)
+        # chat_history = FirestoreChatMessageHistory(
+        #     session_id = SESSION_ID,
+        #     collection = COLLECTION_NAME,
+        #     client = client,
+        # )
+        # chat_history.add_ai_message(system_prompt)
+
+        # for filename in os.listdir("diffs"):
+        #     loader_diff = TextLoader(f"diffs/{filename}")
+        #     diff_documents = loader_diff.load()
+    
+        #     # Split the diff into manageable chunks
+        #     diff_chunks = text_splitter.split_documents(diff_documents)
+    
+        #     review_for_file = ""
+        #     chat_history.add_user_message(f"Please analyze the following diff in the context of the whole app:\n")
+        #     for i, chunk in enumerate(diff_chunks):
+        #         chat_history.add_user_message(chunk.page_content)
+        #     response = chat_history.add_user_message("Can you please review the last diff added")
+        #     chat_history.add_ai_message(response.message)
+
+        
+        system_prompt = """
+        Given a chat history and the latest user question \
+        which might reference context in the chat history, formulate a standalone question \
+        which can be understood without the chat history. Do NOT answer the question, \
+        just reformulate it if needed and otherwise return it as is."""
+        contextualize_q_prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ])
+
+        history_aware_retriever = create_history_aware_retriever(
+            llm_model, retriever, contextualize_q_prompt
+        )
+
+        qa_system_prompt = """You are given a chat history that contains \
+        an entire app with all the files and modules and the user will give you some diff \
+        files. Please analyze these diffs in the context of the entire app, what are the \
         benefits, the drawbacks, if there was a mistake in the logic, how will this \
         change impact the modules that are dependent on this file. Point out any bugs \
         or potential issues that might appear 🐛. Suggest improvements related to \ 
@@ -51,17 +107,18 @@ def generate_feedback():
         Focus on major issues and avoid minor stylistic preferences. \
         Use bullet points for multiple comments. \
         Be specific in your feedback and provide examples if possible."""
-        
-        qa_prompt = ChatPromptTemplate.from_messages([
-            ("system", system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}")
-        ])
-        
-        # Create the RetrievalQA chain for answering questions using the app context
-        retrieval_chain = RetrievalQA.from_chain_type(
-            llm=llm_model, retriever=retriever, chain_type="stuff", prompt=qa_prompt
+
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", qa_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
         )
+        question_answer_chain = create_stuff_documents_chain(llm_model, qa_prompt)
+        rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+        chat_history = []
         
         # Process each diff file and provide feedback
         for filename in os.listdir("diffs"):
@@ -72,15 +129,19 @@ def generate_feedback():
             diff_chunks = text_splitter.split_documents(diff_documents)
     
             review_for_file = ""
+            chat_history.append(HumanMessage(content = "Here starts the diff file:\n"))
             for i, chunk in enumerate(diff_chunks):
-                query = f"Analyze the following diff in the context of the whole app:\n{chunk.page_content}"
-                response = retrieval_chain(query)
-                review_for_file += response["result"]
+                chat_history.append(HumanMessage(content = chunk.page_content))
+            result = rag_chain.invoke({"input": "Please analyse the last diff file that was given to you in the context of the entire app", "chat_history": chat_history})
+
+            chat_history.append(HumanMessage(content = query))
+            chat_history.append(SystemMessage(content = result["answer"]))
     
             # Append the review to the reviews.txt file
             with open("reviews.txt", "a") as output_file:
                 with open(f"diffs/{filename}") as file:
-                    output_file.write(f"FILE: {filename}\nDIFF: {file.read()}\nENDDIFF\nREVIEW: \n{review_for_file}\nENDREVIEW")
+                    output_file.write(f"FILE: {filename}\nDIFF: {file.read()}\nENDDIFF\nREVIEW: \n{result["answer"]}\nENDREVIEW")
+                    
     try:
         get_review()
     except Exception as e:
